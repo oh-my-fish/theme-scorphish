fe70b044c1c78112012dae58e87ea374ef830cc9
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-12-29 21:58:32 +0100

    LICENSE: set correct copyright holder

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,3 +1,12 @@
+# name: scorphish
+
+# This file is part of theme-scorphis
+
+# Licensed under the MIT license:
+# https://opensource.org/licenses/MIT
+# Copyright (c) 2014, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+
 function fish_greeting -d "what's up, fish?"
   set_color $fish_color_autosuggestion[1]
   uname -npsr
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,3 +1,12 @@
+# name: scorphish
+
+# This file is part of theme-scorphis
+
+# Licensed under the MIT license:
+# https://opensource.org/licenses/MIT
+# Copyright (c) 2014, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+
 function fish_right_prompt
   set -l exit_code $status
   if test $exit_code -ne 0
